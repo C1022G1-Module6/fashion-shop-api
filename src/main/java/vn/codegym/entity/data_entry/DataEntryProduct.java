33adbbaba8cce2ca2a217d989a285c7e5ce25777
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-04-25 15:32:14 +0700

    TuNT update DataEntryProduct and DataEntry

---
+++ src/main/java/vn/codegym/entity/data_entry/DataEntryProduct.java
@@ -0,0 +1,58 @@
+package vn.codegym.entity.data_entry;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import vn.codegym.entity.product.Product;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "data_entry_product")
+public class DataEntryProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer quantity;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonBackReference
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "data_entry_id")
+    @JsonBackReference
+    private DataEntry dataEntry;
+
+    public DataEntryProduct() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public DataEntry getDataEntry() {
+        return dataEntry;
+    }
+
+    public void setDataEntry(DataEntry dataEntry) {
+        this.dataEntry = dataEntry;
+    }
+}
