3fb2d0ad65ebdbb73b2a72a9970e69cb5a04a21a
thanhphong8697 <thanhphong861997@gmail.com>
2023-04-26 15:06:03 +0700

    PhongHNT update data entry, data entry product, statistic and JUnit5

--- src/main/java/vn/codegym/controller/product/ProductRestController.java
+++ src/main/java/vn/codegym/controller/product/ProductRestController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.codegym.entity.product.Product;
@@ -20,8 +21,7 @@ import java.util.List;
 
 
 
-
-=======
+@RestController
 @CrossOrigin
 @RequestMapping("api/user/product")
 public class ProductRestController {
