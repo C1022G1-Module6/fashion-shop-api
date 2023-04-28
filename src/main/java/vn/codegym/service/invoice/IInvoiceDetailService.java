2190ea17368167f05592d1a6723456c4087b8cf0
ghgmdr <truongtq20@gmail.com>
2023-04-28 15:40:19 +0700

    TruongTQ cập nhật phần fix Sonarlint thống kê

--- src/main/java/vn/codegym/service/invoice/IInvoiceDetailService.java
+++ src/main/java/vn/codegym/service/invoice/IInvoiceDetailService.java
@@ -4,10 +4,12 @@ import vn.codegym.dto.invoice.InvoiceDetailDTO;
 
 import java.util.List;
 
+
+
 public interface IInvoiceDetailService {
     void save(InvoiceDetailDTO invoiceDetailDTO);
-    //    void update(Integer invoiceId);
     void delete(Integer id);
     List<InvoiceDetailDTO> findAll();
     void deleteAll();
 }
+
