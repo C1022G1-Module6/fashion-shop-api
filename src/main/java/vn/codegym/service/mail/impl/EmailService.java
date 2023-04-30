9d8e01faaaae4a545e4941823ae02e17a8557f15
anhquan1301 <nlaquan1301@gmail.com>
2023-04-30 15:55:33 +0700

    QuanNLA update Employee Detail Fix Comment from Tutor

--- src/main/java/vn/codegym/service/mail/impl/EmailService.java
+++ src/main/java/vn/codegym/service/mail/impl/EmailService.java
@@ -1,17 +1,14 @@
 package vn.codegym.service.mail.impl;
-
 import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorConfig;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import vn.codegym.entity.employee.Employee;
 import vn.codegym.service.employee.IEmployeeService;
 import vn.codegym.service.mail.IEmailService;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
@@ -25,22 +22,29 @@ public class EmailService implements IEmailService {
     public void sendResetPasswordEmail(String email, String otp) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper message = null;
+
         try {
             message = new MimeMessageHelper(mimeMessage, true);
             message.setTo(email);
             message.setSubject("Xác Thực Mã OTP");
-            message.setText(
-                    "Chào bạn," + "\n" +
-                            "Đây là mã OTP của bạn: [ " + otp +" ]" + "\n" +
+            message.setText( "<html>" +
+                    "<body>" +
+                    "<div style=\" font-size:15px;\">"+
+                    "Kính gửi Quý khách hàng," + "<br>" + "<br>" +
+                            "<div style =\" font-weight:bold \"> Đây là mã OTP của bạn: [ " + otp +" ] </div>" + "<br>" +
                             "Mã OTP này sẽ hết hạn trong vòng 1 phút kể từ khi bạn nhận được email này. " +
                             "Vui lòng không chia sẻ mã này với bất kỳ ai, " +
                             "vì nó được sử dụng để xác thực tài khoản của bạn." +
-                            "\n" +
-                            "Nếu bạn không yêu cầu mã OTP này, vui lòng bỏ qua email này hoặc liên hệ với chúng tôi để được hỗ trợ."
-                            + "\n"
-                            + "Trân trọng," + "\n" + "Fashion Shop"
-
-            );
+                            "<br>" +
+                            "Nếu bạn không yêu cầu mã OTP, " +
+                            "vui lòng bỏ qua email này hoặc liên hệ với chúng tôi để được hỗ trợ."
+                            + "<br>"
+                            + "<br>"
+                            + "Trân trọng," +
+                    "<div style =\"color:#183661; font-size:20px ; font-weight:bold\">Fashion Shop</div>" +
+                    "</div>" +
+                    "</body>" +
+                    "</html>", true);
             javaMailSender.send(message.getMimeMessage());
         } catch (MessagingException e) {
             throw new RuntimeException(e);
@@ -65,7 +69,11 @@ public class EmailService implements IEmailService {
 
     @Override
     public String generateOtp(Employee employee) {
-        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+        GoogleAuthenticatorConfig config = new GoogleAuthenticatorConfig
+                .GoogleAuthenticatorConfigBuilder()
+                .setCodeDigits(6)
+                .build();
+        GoogleAuthenticator gAuth = new GoogleAuthenticator(config);
         GoogleAuthenticatorKey key = gAuth.createCredentials();
         String secret = key.getKey();
         employee.setOtpSecret(secret);
@@ -74,5 +82,4 @@ public class EmailService implements IEmailService {
         int code = gAuth.getTotpPassword(secret);
         return Integer.toString(code);
     }
-
 }
