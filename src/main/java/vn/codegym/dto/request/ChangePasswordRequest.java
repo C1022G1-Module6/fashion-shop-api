cc0941252d43f3e94d745b4e7dea4334906c01e2
anhquan1301 <nlaquan1301@gmail.com>
2023-05-01 18:48:52 +0700

    QuanNVL edit gradle

--- src/main/java/vn/codegym/dto/request/ChangePasswordRequest.java
+++ src/main/java/vn/codegym/dto/request/ChangePasswordRequest.java
@@ -4,7 +4,6 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
-
 public class ChangePasswordRequest {
     @Length(min = 5, max = 20, message = "Mật khẩu ít nhất 5 ký tự và nhiều nhất 20 ký tự")
     @NotBlank(message = "Không được bỏ trống")
